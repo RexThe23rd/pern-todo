f6479a887826a770bcd625eb4fda6c5892d6f305
RexThe23rd <rexthe23rd@gmail.com>
2026-01-27 21:45:25 -0500

    started frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
-
+import { use, useState } from "react"
 
 function App() {
+
+  const [description, setDescription] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
+  const [editedText, setEditedText] = useState("")
+
   return (
-    <div>PERN TODO APP</div>
-  )
+    <div className="min-h-screen bg-gray-800 flex justify-center items-center p-4">
+      <div className="bg-gray-50 rounded-2xl shadow-xl w-full max-w-lg p-8">
+        <h1 className="text-4xl font-bold text-gray-800">PERN TODO APP</h1>
+        <form action="">
+          <input type="text" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App
