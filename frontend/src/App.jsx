a684f4318678a62dcc704987f5594dd64dc30879
RexThe23rd <rexthe23rd@gmail.com>
2026-02-04 20:59:53 -0500

    animations finished :D framer motion was interesting but it all worked out

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { MdModeEditOutline, MdOutlineDone } from 'react-icons/md';
 import { FaTrash } from 'react-icons/fa6'
 import { IoClose } from 'react-icons/io5'
+import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
 
@@ -106,7 +107,7 @@ function App() {
   //the app (todo form, displaying the todos, the title, everything basically):
   return (
     <div className="min-h-screen bg-gray-800 flex justify-center items-center p-4">
-      <div className="bg-gray-50 rounded-2xl shadow-xl w-full max-w-lg p-8">
+      <motion.div layout className="bg-gray-50 rounded-2xl shadow-xl w-full max-w-lg p-8">
         <h1 className="text-4xl font-bold text-gray-800 mb-8">PERN TODO APP</h1>
         {error && (
           <div className="bg-red-100 text-red-700 p-3 rounded mb-4">
@@ -129,47 +130,49 @@ function App() {
             <div>
               <p className="text-gray-600">Loading todos...</p>
             </div>
-          ) : todos === 0 ? (
+          ) : todos.length === 0 ? (
             <p className="text-gray-600">Nothing here yet!</p>
           ) : (
             <div className="flex flex-col gap-y-4 bg-blue-50 p-2 rounded-lg">
-              {todos.map((todo) => (
-                <div key={todo.todo_id} className="pb-4">
-                  {editingTodo === todo.todo_id ? (
-                    <div className="flex items-center gap-x-3">
-                      <input className="flex-1 p-3 border-3 rounded-lg border-blue-200 outline-none focus:ring-2 focus:ring-blue-300 text-gray-700 shadow-inner" type="text" value={editedText} onChange={(e) => setEditedText(e.target.value)} />
-                      <div className="flex justify-end items-center">
-                        <button onClick={() => saveEdit(todo.todo_id)} className="px-4 py-2 bg-green-500 text-white rounded-lg mr-2 transition ease-in-out duration-300 hover:bg-green-600"><MdOutlineDone /></button>
-                        <button onClick={() => setEditingTodo(null)} className="px-4 py-2 bg-red-500 text-white rounded-lg mr-2 transition ease-in-out duration-300 hover:bg-red-600"><IoClose /></button>
+              <AnimatePresence>
+                {todos.map((todo) => (
+                  <motion.div layout key={todo.todo_id} className="pb-4" initial={{ opacity: 0, scale: 0.95 }} animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0, scale: 0.95 }} transition={{ duration: 0.15 }}>
+                    {editingTodo === todo.todo_id ? (
+                      <div className="flex items-center gap-x-3">
+                        <input className="flex-1 p-3 border-3 rounded-lg border-blue-200 outline-none focus:ring-2 focus:ring-blue-300 text-gray-700 shadow-inner" type="text" value={editedText} onChange={(e) => setEditedText(e.target.value)} />
+                        <div className="flex justify-end items-center">
+                          <button type="button" onClick={() => saveEdit(todo.todo_id)} className="px-4 py-2 bg-green-500 text-white rounded-lg mr-2 transition ease-in-out duration-300 hover:bg-green-600"><MdOutlineDone /></button>
+                          <button type="button" onClick={() => setEditingTodo(null)} className="px-4 py-2 bg-red-500 text-white rounded-lg mr-2 transition ease-in-out duration-300 hover:bg-red-600"><IoClose /></button>
+                        </div>
                       </div>
-                    </div>
-                  ) : (
-                    <div className="flex justify-between items-center  bg-blue-100 rounded-lg p-1">
-                      <div className="flex items-center gap-x-4 overflow-hidden">
-                        <button onClick={() => toggleCompleted(todo.todo_id)} className={`shrink-0 h-6 w-6 border-2 rounded-full flex items-center justify-center ${todo.completed ? "transition ease-in-out duration-300 bg-green-500 border-green-500 text-white" : "border-gray-400 transition ease-in-out duration-300 hover:border-blue-400"}`}>
-                          {todo.completed && <MdOutlineDone size={16} />}
-                        </button>
-                        <span>{todo.description}</span>
+                    ) : (
+                      <div className="flex justify-between items-center  bg-blue-100 rounded-lg p-1">
+                        <div className="flex items-center gap-x-4 overflow-hidden">
+                          <button type="button" onClick={() => toggleCompleted(todo.todo_id)} className={`shrink-0 h-6 w-6 border-2 rounded-full flex items-center justify-center ${todo.completed ? "transition ease-in-out duration-300 bg-green-500 border-green-500 text-white" : "border-gray-400 transition ease-in-out duration-300 hover:border-blue-400"}`}>
+                            {todo.completed && <MdOutlineDone size={16} />}
+                          </button>
+                          <span>{todo.description}</span>
+                        </div>
+                        <div className="flex gap-x-2">
+                          <button type="button" onClick={() => {
+                            setEditingTodo(todo.todo_id);
+                            setEditedText(todo.description);
+                          }} className="p-2 text-blue-500 bg-blue-200 transition-colors ease-in duration-200 hover:text-blue-700 rounded-lg hover:bg-blue-300">
+                            <MdModeEditOutline />
+                          </button>
+                          <button type="button" onClick={() => { deleteTodo(todo.todo_id) }} className="p-2 text-red-500 bg-red-200 transition-colors ease-in duration-200 hover:text-red-700 rounded-lg hover:bg-red-300">
+                            <FaTrash />
+                          </button>
+                        </div>
                       </div>
-                      <div className="flex gap-x-2">
-                        <button onClick={() => {
-                          setEditingTodo(todo.todo_id);
-                          setEditedText(todo.description);
-                        }} className="p-2 text-blue-500 bg-blue-200 transition-colors ease-in duration-200 hover:text-blue-700 rounded-lg hover:bg-blue-300">
-                          <MdModeEditOutline />
-                        </button>
-                        <button onClick={() => { deleteTodo(todo.todo_id) }} className="p-2 text-red-500 bg-red-200 transition-colors ease-in duration-200 hover:text-red-700 rounded-lg hover:bg-red-300">
-                          <FaTrash />
-                        </button>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              ))}
+                    )}
+                  </motion.div>
+                ))}
+              </AnimatePresence>
             </div>
           )}
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 } // what is this stack of divs
