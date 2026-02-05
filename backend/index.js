8f0bcdee6f69ea8aa0773a007bb5ef9280ed935c
RexThe23rd <rexthe23rd@gmail.com>
2026-02-05 17:29:05 -0500

    backend connection stuff for netlify to work properly

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ app.use(express.json());
 
 app.use("/todos", todoRoutes)
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
